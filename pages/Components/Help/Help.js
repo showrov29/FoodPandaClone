d7cd0dd5f3b2dbc1451370fac8c7f66f9b1ca219
Akramuzzaman-Raju <akraju012@gmail.com>
2023-10-31 20:07:51 +0600

    up

--- pages/Components/Help/Help.js
+++ pages/Components/Help/Help.js
@@ -18,7 +18,7 @@ const Help = () => {
   return (
     <>
     <div className='bg-white'>
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '70vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '75vh' }}>
       <div className='flex justify-between pt-6 px-4'>
         <div className='flex justify-start font-normal text-lg '>
           <div className=''>
